0843988f1044279ad7f782276614670e12140259
EGlebov <EGlebov@synergy.ru longpaths = true>
2019-04-02 12:13:40 +0300

    slick

--- public/js/script.js
+++ public/js/script.js
@@ -103,10 +103,10 @@ $(function () {
 	lazyLoad: 'ondemand',
 	arrows: false,
 	/*nextArrow: '<div class="slick-next">&#8669;</div>',
-	prevArrow: '<div class="slick-prew">&#8668;</div>',*/
+	prevArrow: '<div class="slick-prew">&#8668;</div>',*/	
 	responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 1200,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 3,
@@ -115,7 +115,7 @@ $(function () {
       }
     },
     {
-      breakpoint: 600,
+      breakpoint: 800,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 2
@@ -125,13 +125,12 @@ $(function () {
       breakpoint: 480,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        centerMode: true,
+ 		centerPadding: '5px'
       }
     }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
+  	] 
 
 	  });
 	}
