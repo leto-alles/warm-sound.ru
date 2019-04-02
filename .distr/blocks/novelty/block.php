3dcf0c603c8186d7e1ee2d9e078229fd8f9baa86
EGlebov <EGlebov@synergy.ru longpaths = true>
2019-04-02 14:09:35 +0300

    div

--- .distr/blocks/novelty/block.php
+++ .distr/blocks/novelty/block.php
@@ -5,18 +5,20 @@
         <h2 class="section-title novelty-title">Новые поступления</h2>
         <div class="novelty_block slick-novelty"> 
         {% for item in noveltyItems %}    
-
+        <a href="#novelty-popap" class="fansybox">
             <div class="novelty_block-item">
                 <img data-lazy="{{ item.photo_main }}"  alt="{{ item.photo_alt }}" class="novelty_block-img vinyl-img">
                 <h3 class="novelty_block-title vinyl-title">{{ item.title_artist }}</h3>
                 <p class="novelty_block-line"></p>
                 <p class="novelty_block-album vinyl-album">{{ item.title_album }}</p>
             </div>  
-
+        </a>    
 
         {% endfor %}  
         </div>
     </div>
 </section>
 
+
+<div id="#novelty-popap" class= "target"></div>
 
--- public/index.php
+++ public/index.php
@@ -61,80 +61,82 @@ include_once 'prices.php';
         <h2 class="section-title novelty-title">Новые поступления</h2>
         <div class="novelty_block slick-novelty"> 
             
-
+        <a href="#novelty-popap" class="fansybox">
             <div class="novelty_block-item">
                 <img data-lazy="/img/novelty/beatles/white/1.jpg"  alt="White Album The Beatles LP" class="novelty_block-img vinyl-img">
                 <h3 class="novelty_block-title vinyl-title">The Beatles lkbyyjt yfpdfbyt fkm,jvf vf</h3>
                 <p class="novelty_block-line"></p>
                 <p class="novelty_block-album vinyl-album">White Album</p>
             </div>  
-
+        </a>    
 
             
-
+        <a href="#novelty-popap" class="fansybox">
             <div class="novelty_block-item">
                 <img data-lazy="/img/novelty/beatles/white/2.jpg"  alt="White Album The Beatles LP" class="novelty_block-img vinyl-img">
                 <h3 class="novelty_block-title vinyl-title">The Beatles</h3>
                 <p class="novelty_block-line"></p>
                 <p class="novelty_block-album vinyl-album">White Album</p>
             </div>  
-
+        </a>    
 
             
-
+        <a href="#novelty-popap" class="fansybox">
             <div class="novelty_block-item">
                 <img data-lazy="/img/novelty/beatles/white/3.jpg"  alt="White Album The Beatles LP" class="novelty_block-img vinyl-img">
                 <h3 class="novelty_block-title vinyl-title">The Beatles</h3>
                 <p class="novelty_block-line"></p>
                 <p class="novelty_block-album vinyl-album">White Album</p>
             </div>  
-
+        </a>    
 
             
-
+        <a href="#novelty-popap" class="fansybox">
             <div class="novelty_block-item">
                 <img data-lazy="/img/novelty/beatles/white/4.jpg"  alt="White Album The Beatles LP" class="novelty_block-img vinyl-img">
                 <h3 class="novelty_block-title vinyl-title">The Beatles</h3>
                 <p class="novelty_block-line"></p>
                 <p class="novelty_block-album vinyl-album">White Album</p>
             </div>  
-
+        </a>    
 
             
-
+        <a href="#novelty-popap" class="fansybox">
             <div class="novelty_block-item">
                 <img data-lazy="/img/novelty/beatles/white/1.jpg"  alt="White Album The Beatles LP" class="novelty_block-img vinyl-img">
                 <h3 class="novelty_block-title vinyl-title">The Beatles</h3>
                 <p class="novelty_block-line"></p>
                 <p class="novelty_block-album vinyl-album">White Album</p>
             </div>  
-
+        </a>    
 
             
-
+        <a href="#novelty-popap" class="fansybox">
             <div class="novelty_block-item">
                 <img data-lazy="/img/novelty/beatles/white/2.jpg"  alt="White Album The Beatles LP" class="novelty_block-img vinyl-img">
                 <h3 class="novelty_block-title vinyl-title">The Beatles</h3>
                 <p class="novelty_block-line"></p>
                 <p class="novelty_block-album vinyl-album">White Album</p>
             </div>  
-
+        </a>    
 
             
-
+        <a href="#novelty-popap" class="fansybox">
             <div class="novelty_block-item">
                 <img data-lazy="/img/novelty/beatles/white/3.jpg"  alt="White Album The Beatles LP" class="novelty_block-img vinyl-img">
                 <h3 class="novelty_block-title vinyl-title">The Beatles</h3>
                 <p class="novelty_block-line"></p>
                 <p class="novelty_block-album vinyl-album">White Album</p>
             </div>  
-
+        </a>    
 
           
         </div>
     </div>
 </section>
 
+
+<div id="#novelty-popap" class= "target"></div>
 
 
 
